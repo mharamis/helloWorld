844d15b770940d1b6d1975bc9c35fdb74279d63f
Michael Haramis <mharamis@tepmail.umd.edu>
2024-02-19 17:29:57 -0500

    added code for /about route

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World from Michael Haramis!'
 
+@app.route('/hello')
+def hello_world2():
+    return render_template('hello.html')
+
 @app.route('/about')
-def about():  # put application's code here
+def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
