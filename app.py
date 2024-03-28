0c51a520123f55f3f4123473660f92004c23e921
Michael Haramis <mharamis@tepmail.umd.edu>
2024-03-28 17:12:11 -0400

    added /favorite-course route and /contact route
    added favorite-course.html that takes in course_subject and course_subject
    
    added base.html that uses bootstrap formatting
    
    added contact.html that creates a contact form and notifies user when info is submitted

--- app.py
+++ app.py
@@ -15,14 +15,5 @@ def hello_world2():
 def about():
     return render_template('about.html')
 
-@app.route('/about-css')
-def aboutcss():
-    return render_template('about-css.html')
-
-@app.route('/profit-summary')
-def aboutcss():
-    return render_template('profit-summary.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
